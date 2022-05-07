9fc6a3aa0267d27703bfb33160bf4f657db86643
mat <psaurina@ucm.es>
2022-05-07 13:20:03 +0200

    2 arreglado

--- Apartado2.py
+++ Apartado2.py
@@ -3,8 +3,8 @@ import sys
 
 def mapper(line):
     edge = line.split(',')
-    n1 = edge[0][1:-1]
-    n2 = edge[1][1:-1]
+    n1 = edge[0]
+    n2 = edge[1]
     return [(n1,n2), (n2,n1)]
 
 SAMPLE = 15
@@ -12,12 +12,13 @@ SAMPLE = 15
 sc = SparkContext()
 
 rdd1 = sc.textFile(sys.argv[1])
-print('textFile', rdd.take(SAMPLE))
+print('textFile1', rdd1.take(SAMPLE))
 
 rdd2 = sc.textFile(sys.argv[2])
-print('textFile', rdd.take(SAMPLE))
+print('textFile2', rdd2.take(SAMPLE))
 
-rdd = rdd2.union(rdd1).reduceByKey(lambda x,y : x+y)
+rdd = rdd2.union(rdd1)
+print('mergedTextFiles',rdd.take(SAMPLE))
 
 rdd = rdd.flatMap(mapper)
 print('flatMap', rdd.take(SAMPLE))
@@ -31,20 +32,19 @@ print('distinct', rdd.take(SAMPLE))
 rdd = rdd.groupByKey()
 print('groupByKey', rdd.take(SAMPLE))
 
-#Este es el paso más técnico:
-#    1º necesitamos que la lista de vecinos esté ordenada para
-#    así poder comparar con otras listas de vecinos;
-#    2º para que pueda ser clave, tenemos que convertir la lista en tupla.
-rdd = rdd.map(lambda x: (tuple(sorted(x[1])), x[0]))
+rdd = rdd.map(lambda x: (x[0],tuple(sorted(x[1]))))
 print('map', rdd.take(SAMPLE))
-
-rdd = rdd.groupByKey()
-print('Result:')
-resultados = False
-for adyacentes, nodos in rdd.collect():
-    vertices = list(nodos)
-    if len(vertices)>1:
-        resultados = True
-        print("vertices", list(nodos), "has common adjacents", adyacentes)
-if not resultados:
-    print ('No vertices with common adjacents, try another graph!')
+lista = []
+for i,j in rdd.collect():
+    for k,l in rdd.collect():
+        if k in j and i in l:
+            for m in j:
+                if m in l:
+                    lista.append(tuple(sorted([i,k,m])))
+print(lista)
+res = sc.parallelize(lista)
+res = res.distinct()
+print('res',res.take(SAMPLE))
+print("Result:")
+for i in res.collect():
+    print("Los vertices",i,"forman un 3-ciclo.")
