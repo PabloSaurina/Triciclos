a2451ff9ac80103d89bcbe65dc393661e27287dd
mat <psaurina@ucm.es>
2022-05-07 13:41:57 +0200

    3 arreglado

--- Apartado3.py
+++ Apartado3.py
@@ -4,8 +4,8 @@ from multiprocessing import Process
 
 def mapper(line):
     edge = line.split(',')
-    n1 = edge[0][1:-1]
-    n2 = edge[1][1:-1]
+    n1 = edge[0]
+    n2 = edge[1]
     return [(n1,n2), (n2,n1)]
 
 SAMPLE = 15
@@ -13,36 +13,39 @@ SAMPLE = 15
 sc = SparkContext()
 
 
-def task(file):
-    rdd = sc.textFile(file)
-    print('textFile', rdd.take(SAMPLE))
+def task(ide,rdd,sc):
     rdd = rdd.flatMap(mapper)
-    print('flatMap', rdd.take(SAMPLE))
+#    print('flatMap', rdd.take(SAMPLE))
     rdd = rdd.filter(lambda x: x[0]!=x[1])
-    print('filter', rdd.take(SAMPLE))
+#    print('filter', rdd.take(SAMPLE))
     rdd = rdd.distinct()
-    print('distinct', rdd.take(SAMPLE))
+#    print('distinct', rdd.take(SAMPLE))
     rdd = rdd.groupByKey()
-    print('groupByKey', rdd.take(SAMPLE))
-    rdd = rdd.map(lambda x: (tuple(sorted(x[1])), x[0]))
-    print('map', rdd.take(SAMPLE))
-    rdd = rdd.groupByKey()
-    print('Result:')
-    resultados = False
-    for adyacentes, nodos in rdd.collect():
-        vertices = list(nodos)
-        if len(vertices)>1:
-            resultados = True
-            print("vertices", list(nodos), "has common adjacents", adyacentes)
-    if not resultados:
-        print ('No vertices with common adjacents, try another graph!')
+#    print('groupByKey', rdd.take(SAMPLE))
+    rdd = rdd.map(lambda x: (x[0],tuple(sorted(x[1]))))
+#    print('map', rdd.take(SAMPLE))
+    lista = []
+    for i,j in rdd.collect():
+        for k,l in rdd.collect():
+            if k in j and i in l:
+                for m in j:
+                    if m in l:
+                        lista.append(tuple(sorted([i,k,m])))
+#    print(lista)
+    res = sc.parallelize(lista)
+    res = res.distinct()
+#    print('res',res.take(SAMPLE))
+    for i in res.collect():
+        print("Los vertices",i,f"forman un 3-ciclo en el archivo {ide}.")
 
 
 def main():
     lp = []
-    for i in range(len(sys.argv) - 1):
-       	file = sys.argv[i+1]
-        lp.append(Process(target=task,args = (file)))
+    n = len(sys.argv)
+    for i in range( n- 1):
+        rdd = sc.textFile(sys.argv[i+1])
+        print(f'textFile {i+1}', rdd.take(SAMPLE))
+        lp.append(Process(target=task,args = (i+1,rdd,sc)))
     for p in lp:
         p.start()
     for p in lp:
